4b43739f5163006c7ddf1128bb0358af4e5ce1b2
Roman <romantimilsina@gmail.com>
2023-11-08 11:24:11 +0545

    Initial Commit

---
+++ PractiseSwiftUI/PractiseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractiseSwiftUIApp.swift
+//  PractiseSwiftUI
+//
+//  Created by Mac on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PractiseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
